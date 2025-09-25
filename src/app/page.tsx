35e9ede9dc960bc3996aeea10d6ba09d9a2d7de8
simon <simon@spaced.be>
2025-09-25 15:17:24 +0200

    fix some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,17 @@ export default function Home() {
       </div>
 
       <InfiniteImageScroller />
+
+      <div className="flex w-full justify-between align-center p-8">
+        <h1 className="text-4xl font-bold">
+          Free palestine.
+        </h1>
+
+        <div className="flex gap-4">
+          <a href="mailto:info@designisavoice.com">participate</a>
+          <a href="mailto:info@designisavoice.com">donate</a>
+        </div>
+      </div>
     </div>
   );
 }
